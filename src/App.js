ecbb726a3d1c3c5d54ead1ec4b2a2fd9031f280e
Abhinav <abhinavluv19@gmail.com>
2020-12-24 20:13:45 +0530

    Fetch Users using API, add Spinner

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 import Navbar from './components/Layout/Navbar/Navbar.component';
 import Users from './components/Users/Users/Users.component';
 
 class App extends Component {
+    state = {
+        users: [],
+        loading: false,
+    };
+
+    async componentDidMount() {
+        this.setState({ loading: true });
+        const response = await axios.get(
+            `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
+        this.setState({ users: response.data, loading: false });
+    }
+
     render() {
         return (
             <div className='App'>
                 <Navbar title='Github Finder' icon='fab fa-github' />
                 <div className='container'>
-                    <Users />
+                    <Users
+                        loading={this.state.loading}
+                        users={this.state.users}
+                    />
                 </div>
             </div>
         );
--- src/components/Users/Users/Users.component.jsx
+++ src/components/Users/Users/Users.component.jsx
@@ -1,45 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import UserItem from '../UserItem/UserItem.component';
+import Spinner from '../../Layout/Spinner/Spinner.component';
 
-export class Users extends Component {
-    state = {
-        users: [
-            {
-                login: 'mojombo',
-                id: 1,
-                avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-                html_url: 'https://github.com/mojombo',
-            },
-            {
-                login: 'defunkt',
-                id: 2,
-                avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-                html_url: 'https://github.com/defunkt',
-            },
-            {
-                login: 'pjhyett',
-                id: 3,
-                avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-                html_url: 'https://github.com/pjhyett',
-            },
-        ],
-    };
-
-    userStyle = {
-        display: 'grid',
-        gridTemplateColumns: 'repeat(3, 1fr',
-        gridGap: '1rem',
-    };
-
-    render() {
+const Users = (props) => {
+    if (props.loading) {
+        return <Spinner />;
+    } else {
         return (
-            <div style={this.userStyle}>
-                {this.state.users.map((user) => {
+            <div style={userStyle}>
+                {props.users.map((user) => {
                     return <UserItem key={user.id} user={user} />;
                 })}
             </div>
         );
     }
-}
+};
+
+const userStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr',
+    gridGap: '1rem',
+};
+
+Users.propTypes = {
+    loading: PropTypes.bool.isRequired,
+    users: PropTypes.array.isRequired,
+};
 
 export default Users;
